922c82acbcee8fbbc4ed813756f276f77145d8b5
Philip Jägenstedt <philip@foolip.org>
2013-04-19 14:25:36 +0700

    Support reading the image size for a .size file
    At the same time, drop support for .args, which won't be needed.

--- resize.sh
+++ resize.sh
@@ -8,13 +8,14 @@ maxw="$3"
 maxh="$4"
 
 identify -format "%w %h" "$src" | while read w h; do
-    argf="${src%.*}.args"
     crop="${src%.*}.crop"
-    if [ -e "$argf" ]; then
-	read args < "$argf"
-    elif [ -e "$crop" ]; then
+    size="${src%.*}.size"
+    if [ -e "$crop" ]; then
 	read top right bottom left < "$crop"
 	args="-crop $(($w-$left-$right))x$(($h-$top-$bottom))+$left+$top"
+    fi
+    if [ -e "$size" ]; then
+	read maxw maxh < "$size"
     fi
     if [ -n "$args" -o $w -gt $maxw -o $h -gt $maxh ]; then
 	convert "$src" $args +repage -resize "${maxw}x${maxh}>" \
