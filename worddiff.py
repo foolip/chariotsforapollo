f858ab40590aa9a72c77900bc9fe2352ef22b2bf
Philip Jägenstedt <philip@foolip.org>
2013-04-29 11:45:04 +0700

    Handle HTML in worddiff.py

--- worddiff.py
+++ worddiff.py
@@ -4,10 +4,35 @@
 # Get words from HTML by including worddiff.js
 
 import difflib
+import os.path
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]),
+                             'html5lib-python'))
+import html5lib
+
 def getwords(path):
-    return open(path, 'r').read().split()
+    f = open(path, 'r')
+    if path.endswith('.htm') or path.endswith('.html'):
+        doc = html5lib.parse(f, treebuilder='dom')
+        body = doc.documentElement.lastChild
+        assert body.tagName == 'body'
+        return ''.join(itertext(body)).encode('utf-8').split()
+    else:
+        return f.read().split()
+
+def itertext(node):
+    if node.nodeType == node.TEXT_NODE:
+        yield node.data.replace(u'\u00AD', '')
+    elif node.nodeType == node.ELEMENT_NODE:
+        showTag = node.tagName in ['b', 'i', 'sub', 'sup']
+        if showTag:
+            yield '<%s>' % node.tagName
+        for child in node.childNodes:
+            for text in itertext(child):
+                yield text
+        if showTag:
+            yield '</%s>' % node.tagName
 
 a = getwords(sys.argv[1])
 b = getwords(sys.argv[2])
